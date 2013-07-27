eac053ab0a09766749fb4284bbd5a7c8d1c220e1
Ryan Kadwell <ryan@riaka.ca>
2013-07-26 22:17:38 -0400

    Added license info

--- src/Pandoc/PandocException.php
+++ src/Pandoc/PandocException.php
@@ -1,7 +1,11 @@
 <?php
 /**
  * Pandoc PHP
- * Copyright (c) Ryan Kadwell <ryan@pause.ca>
+ *
+ * Copyright (c) Ryan Kadwell <ryan@riaka.ca>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Pandoc;
